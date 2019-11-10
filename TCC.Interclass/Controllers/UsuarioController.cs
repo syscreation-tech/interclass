456a1dc75a96a88ae7f615ef6e2c273a5873664f
syscreation-tech <syscreation6@gmail.com>
2019-11-10 14:41:17 -0300

    Subindo outros fontes

---
+++ TCC.Interclass/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCC.Interclass.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
