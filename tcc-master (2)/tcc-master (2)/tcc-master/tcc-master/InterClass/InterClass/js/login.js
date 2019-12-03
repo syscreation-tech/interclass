d324f01d8b3c19a9588aeff2120cf9b545a4cda8
syscreation-tech <syscreation6@gmail.com>
2019-12-03 07:55:16 -0300

    animação

---
+++ tcc-master (2)/tcc-master (2)/tcc-master/tcc-master/InterClass/InterClass/js/login.js
@@ -0,0 +1,26 @@
+﻿const ulSquares = document.querySelector("ul.squares");
+for (let i = 0; i < 11; i++) {
+    const li = document.createElement("li")
+    const random = (min, max)=> Math.random() * (max - min) + min;
+    const size = Math.floor(random(10 - 120));
+    const position = random(1, 99);
+    const delay = random(5, 0.1);
+    const duration = random(24, 12);
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+    li.style.left = `${position}%`;
+    //    li.style.width =  ';
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${delay}s`;
+    ulSquares.appendChild(li);
+
+}
+alert();
+const btnLogin = document.querySelector('.btn-login');
+const form = document.querySelector("form");
+
+btnLogin.addEventListener("click", event => {
+    event.preventDefault();
+    form.classList.add("form-hide");
+}); 
